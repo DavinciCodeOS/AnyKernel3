6847c77d038d866a9bd3cdc073b2708713b49d99
Jens Reidel <adrian@travitia.xyz>
2022-11-19 23:50:09 +0100

    Update name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=VantomLillia by Adrian and Steve based on Vantomkernel
+kernel.string=VantomSyndra by Team Kenvyra based on Vantomkernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
